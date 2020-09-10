8a8e27fb4360cf8032aaa2e866dc8d05c3b2bb2c
lwchao <lwchao89@outlook.com>
2020-09-10 17:10:00 +0800

    Update bsearch.c

--- bsearch.c
+++ bsearch.c
@@ -15,16 +15,13 @@ struct item items[5] = {
 {.a = 5, .b = 1,.c= "hello5"}
 };
 
-
+// 比较函数应该返回 大于0 小于0 ，才能实现二分查找
 static int comp_str(const void *a, const void *b)
 {
         struct item *aitem = (struct item *)a;
         struct item *bitem = (struct item *)b;
 
-        if (aitem->a == bitem->a)
-                return 0;
-
-        return -1;
+        return aitem->a - bitem->a;
 }
 
 
